698aa7747d82f581e1944cb670e8d78a232932d2
Julian Correa <juliangcorrea@hotmail.com>
2025-01-15 16:56:37 -0300

    Finish about section

--- src/aboutModule.js
+++ src/aboutModule.js
@@ -1,8 +1,16 @@
 export default function addMenu(){
     const mainContainer = document.getElementById('content')
     mainContainer.innerHTML = ''
-    const aboutSectionContent = ``
+    const aboutSectionContent = `<div class="title">
+                <h2>Contact Us</h2>
+            </div>
+            <div class="contact-info">
+                <h2>Name of contact</h2>
+                <h3>Position of contact</h3>
+                <h3>Phone Number: xxx-xxx-xxxx</h3>
+                <h3>Email: sampleEmail@sample.com</h3>
+            </div>`
     const aboutSection = document.createElement('section')
-    aboutSection.innerText = aboutSectionContent
+    aboutSection.innerHTML = aboutSectionContent
     mainContainer.appendChild(aboutSection)
 }
